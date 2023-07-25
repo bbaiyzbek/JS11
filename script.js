979b0c579d53878c55b8de5afceaae016f606162
BaQuna <imbaquna@gmail.com>
2023-07-25 23:18:48 +0600

    added Population, Area and button Google map

--- script.js
+++ script.js
@@ -17,8 +17,13 @@ const handleGetFlags = () => {
                 <div class="col-4">
                      <div class="box">
                          <img src=${country.flags.png} alt="">
-                         <h3 class="p-3">${country.name.official}</h3>
-                         <p>${country.capital}</p>             
+                         <h3 class="p-1">${country.name.official}</h3>
+                         <p>${country.capital}</p> 
+                         <p>Population: ${country.population}</p>
+                         <p>Area: ${country.area} km2</p>
+                         <button class="mapBtn p-1 btn-outline-info bg-light border-light">
+                         <a class="text-decoration-none text-dark" href="${country.maps.googleMaps}">Google Map</a>
+                         </button>          
                      </div>      
                 </div>
                 `
